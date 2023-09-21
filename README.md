b97fcffc5b4de275ba0f5ca5dfe3704697439c80
Thomas Boerger <thomas@webhippie.de>
2023-09-21 14:57:04 +0200

    feat: add workflow to manage memberships and labels

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Cloudhippie: GitHub
+
+[![General Workflow](https://github.com/cloudhippie/.github/actions/workflows/general.yml/badge.svg)](https://github.com/cloudhippie/.github/actions/workflows/general.yml)
+
+TBD
+
+## Development
+
+TBD
+
+## Security
+
+If you find a security issue please contact
+[security@cloudhippie.de](mailto:security@cloudhippie.de) first.
+
+## Contributing
+
+Fork -> Patch -> Push -> Pull Request
+
+## Authors
+
+-   [Thomas Boerger](https://github.com/tboerger)
+
+## License
+
+Apache-2.0
+
+## Copyright
+
+```console
+Copyright (c) 2023 Cloudhippie <info@cloudhippie.de>
+```
---
+++ terraform/terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "s3" {
+    bucket                      = "cloudhippie-terraform"
+    key                         = "github/github"
+    region                      = "auto"
+    endpoint                    = "https://3c268ef9f938c1bea56d66644437edcc.r2.cloudflarestorage.com"
+    force_path_style            = true
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_metadata_api_check     = true
+  }
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.37.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+
+provider "github" {
+  owner = "cloudhippie"
+}
