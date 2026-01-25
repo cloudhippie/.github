5ef6ac5fea2a884adc90a275dac7f8c828447f2a
Thomas Boerger <thomas@webhippie.de>
2026-01-25 22:32:23 +0100

    chore: replace nixfmt package

--- flake.nix
+++ flake.nix
@@ -57,7 +57,7 @@
               default = {
                 git-hooks = {
                   hooks = {
-                    nixfmt-rfc-style = {
+                    nixfmt = {
                       enable = true;
                     };
                   };
@@ -65,7 +65,7 @@
 
                 packages = with pkgs; [
                   _1password-cli
-                  nixfmt-rfc-style
+                  nixfmt
                 ];
               };
             };
